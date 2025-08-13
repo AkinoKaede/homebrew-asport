c3a6cf4d8ed79a4740d62f9906548808ab7094f0
Kaede Akino <i@akinokae.de>
2025-08-13 12:43:25 +0800

    asport-server 0.3.0

--- Formula/asport-server.rb
+++ Formula/asport-server.rb
@@ -1,8 +1,8 @@
 class AsportServer < Formula
   desc "A quick and secure reverse proxy server based on QUIC for NAT traversal."
   homepage "https://github.com/AkinoKaede/asport"
-  url "https://github.com/AkinoKaede/asport/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "f4a33fda5db89e5e750fe419aff43e00dbe0a04c1a86f5b7b46ff6c5ddeecedf"
+  url "https://github.com/AkinoKaede/asport/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "7d994546763cc7125247dc2e043cbfe7c3f158528d0ee0d8d5a2db8c2af58c09"
   license "GPL-3.0-or-later"
   head "https://github.com/AkinoKaede/asport", branch: "main"
 
@@ -15,7 +15,7 @@ class AsportServer < Formula
   end
 
   service do
-    run [opt_bin/"asport-server", "#{etc}/asport/server.toml"]
+    run [opt_bin/"asport-server", "run", "--config", "#{etc}/asport/server.toml"]
     keep_alive true
     log_path var/"log/asport-server.log"
     error_log_path var/"log/asport-server.log"
