a8bebba7b8c976d78fa5140b37e4647a4bbd9412
Kaede Akino <i@akinokae.de>
2025-08-13 13:07:51 +0800

    asport-client: fix sha256

--- Formula/asport-client.rb
+++ Formula/asport-client.rb
@@ -2,7 +2,7 @@ class AsportClient < Formula
   desc "A quick and secure reverse proxy client based on QUIC for NAT traversal."
   homepage "https://github.com/AkinoKaede/asport"
   url "https://github.com/AkinoKaede/asport/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "f4a33fda5db89e5e750fe419aff43e00dbe0a04c1a86f5b7b46ff6c5ddeecedf"
+  sha256 "7d994546763cc7125247dc2e043cbfe7c3f158528d0ee0d8d5a2db8c2af58c09"
   license "GPL-3.0-or-later"
   head "https://github.com/AkinoKaede/asport", branch: "main"
 
